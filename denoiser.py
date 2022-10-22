eebcec534b11d162762a9e89e64515c3d57801b4
Rodrigofth <60795329+Rodrigofth@users.noreply.github.com>
2022-10-22 18:11:30 -0300

    Update denoiser.py

--- denoiser.py
+++ denoiser.py
@@ -241,8 +241,9 @@ def Mask_Ideal_Bin(specV, specN, snr):
 
 def Get_Features():
 #Baxa os arquivos e downsample audios
+  print("Audios")
   Voice = Get_Audio()
-  
+  print("Ruidos")
   Noise = Get_Noise()
 #Mixa audios limpos com os segmentos equivalentes dos ruidos
   i = 0
